b2a6b6eecc12af7baf29b921efbd66d843697889
duansheng <duansheng@iauto.com>
2020-09-26 22:40:32 +0800

    Simplify scanner.rkt

--- scanner.rkt
+++ scanner.rkt
@@ -15,6 +15,8 @@
     (define str chars)
     (define start 0)
     (define line 1)
+    (define c (peek))
+    (define prev #f)
     (define keywords (make-hash))
 
     (let ([keys '(and class else false fun for if nil or
@@ -28,43 +30,39 @@
           (string-ref str pos)))
 
     (define/public (next)
-      (when (eqv? #\newline (peek))
+      (when (eqv? #\newline c)
         (set! line (add1 line)))
-      (set! pos (add1 pos)))
+      (set! pos (add1 pos))
+      (set! prev c)
+      (if (>= pos (string-length str))
+          (set! c eof)
+          (set! c (string-ref str pos))))
 
-    (define/public (_match c)
-      (if (eqv? c (peek))
-          (next)
-          #f))
+    (define/public (_match ch)
+      (and (eqv? ch c) (next)))
 
     (define/public (_text)
       (substring str start pos))
 
     (define/public (numeric?)
-      (let ([c (peek)])
-        (and (char? c) (char-numeric? c))))
+      (and (char? c) (char-numeric? c)))
 
     (define/public (name-start?)
-      (let ([c (peek)])
-        (and (char? c) (or (eqv? c #\_) (char-alphabetic? c)))))
+      (and (char? c) (or (eqv? c #\_) (char-alphabetic? c))))
 
     (define/public (name-char?)
       (or (name-start?) (numeric?)))
 
     (define/public (whitespace?)
-      (let ([c (peek)])
-        (and (char? c) (char-whitespace? c))))
+      (and (char? c) (char-whitespace? c)))
 
     (define/public (make-string-token)
-      (define c (peek))
       (while (not (or (eqv? c eof) (eqv? c #\")))
-        (next)
-        (set! c (peek)))
+        (next))
       (next) ; skip right "
       (token 'string line (_text)))
 
     (define/public (make-number-token)
-      (define c (peek))
       (while (numeric?)
         (next))
       (token 'number line (string->number (_text))))
@@ -81,6 +79,10 @@
       (while (whitespace?)
         (next)))
 
+    (define/public (skip-comment)
+      (while (not (or (eqv? c eof) (eqv? c #\newline)))
+        (next)))
+
     (define/public (get-tokens)
       (let ([tokens '()]
             [tok (tokenize)])
@@ -91,31 +93,20 @@
         (reverse tokens)))
 
     (define/public (tokenize)
-      (define c (peek))
-
       (skip-white)
       (set! start pos)
-      (set! c (peek))
       (cond [(eof-object? c) (empty-token 'eof line)]
             [(numeric?) (make-number-token)]
             [(name-start?) (make-id-token)]
             [(eqv? c #\") (next) (make-string-token)]
             [else
              (case (char-to-symbol c)
-               [(+ - * |.| |,| |;| |(| |)| |{| |}|) (next) (empty-token (char-to-symbol c) line)]
-               [(= < > !)
-                (next)
-                (if (_match #\=)
-                    (empty-token (string->symbol (_text)) line)
-                    (empty-token (char-to-symbol c) line))]
-               [(/)
-                (next)
-                (let ([c (peek)])
-                  (if (_match #\/)
-                      (begin
-                        (while (not (or (eqv? c eof) (eqv? c #\newline)))
-                          (next)
-                          (set! c (peek)))
-                        (tokenize))
-                      (empty-token '/ line)))]
+               [(+ - * |.| |,| |;| |(| |)| |{| |}|) (next) (empty-token (char-to-symbol prev) line)]
+               [(= < > !) (next)
+                          (if (_match #\=)
+                              (empty-token (string->symbol (_text)) line)
+                              (empty-token (char-to-symbol prev) line))]
+               [(/) (next)
+                    (cond [(_match #\/) (skip-comment) (tokenize)]
+                          [else (empty-token '/ line)])]
                [else (raise (lex-exn (format "Invalid character '~a'" c) (current-continuation-marks)))])]))))
